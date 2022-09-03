bdb3f5431a2ee0cd6d3204da3b73ee96dcb4a750
Renê <renejr.arraes286@gmail.com>
2022-09-03 14:34:27 -0300

    feat: add options button in the card

--- frontend/src/components/class-card/class-card.jsx
+++ frontend/src/components/class-card/class-card.jsx
@@ -2,17 +2,69 @@ import React from "react";
 import { theme } from "./../../theme.js";
 import { ThemeProvider } from "@emotion/react";
 import { Box } from "@mui/material";
-import { Card, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Tooltip,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export function ClassCard(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menu = (
+    <Menu
+      anchorEl={anchorEl}
+      id="account-menu"
+      open={open}
+      onClose={handleClose}
+      onClick={handleClose}
+    >
+      <MenuItem>Remover</MenuItem>
+      <MenuItem>Pré-requisitos</MenuItem>
+      <MenuItem>Pós-requisitos</MenuItem>
+    </Menu>
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <Card
-      >
+      <Card>
         <CardContent>
-          <Typography sx={{ fontSize: 16 }} color="primary">
-            {props.name}
-          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography sx={{ fontSize: 16 }} color="primary">
+              {props.name}
+            </Typography>
+            <Tooltip title="Opções">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                aria-controls={open ? `account-menu-${props.id}` : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <MoreHorizIcon color="primary" />
+              </IconButton>
+            </Tooltip>
+            {menu}
+          </Box>
           <Box
             sx={{
               display: "flex",
