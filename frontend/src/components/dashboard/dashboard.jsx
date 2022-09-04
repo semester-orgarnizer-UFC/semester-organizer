8a2014f75a4c7eda973302faa4af6792eb9aed6b
Renê <renejr.arraes286@gmail.com>
2022-09-03 21:41:39 -0300

    feat: add the classes not taken in the dashboard

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -1,34 +1,38 @@
 import React from "react";
 import "./dashboard.css";
-import { Card, CardContent, Typography, Box, Container } from "@mui/material";
+import { Box, Fab, ThemeProvider } from "@mui/material";
 import CardWrap from "./../card-wrap/card-wrap.jsx";
 import ClassesNotTaken from "../classes-not-taken/classes-not-taken";
+import AddIcon from "@mui/icons-material/Add";
+import { theme } from "./../../theme.js";
 
 const data = [
-    {
-      name: "Fundamentos de programação",
-      id: "QXD0001",
-      hours: 64,
-    },
-    {
-      name: "Programção orientada a objetos",
-      id: "QXD0007",
-      hours: 64,
-    },
-  ];
+  {
+    name: "Fundamentos de programação",
+    id: "QXD0001",
+    hours: 64,
+  },
+  {
+    name: "Programção orientada a objetos",
+    id: "QXD0007",
+    hours: 64,
+  },
+];
 const Dashboard = () => (
-  <Box className="dashboard">
-    <Box
-      sx={{
-        display: "flex",
-        gap: "10px",
-      }}
-    >
-      <CardWrap index={1} data={data} />
-      <CardWrap index={2} data={data} />
-      <ClassesNotTaken />
+  <ThemeProvider theme={theme}>
+    <Box className="dashboard">
+      <Box
+        sx={{
+          display: "flex",
+          gap: "10px",
+        }}
+      >
+        <CardWrap index={1} data={data} />
+        <CardWrap index={2} data={data} />
+        <ClassesNotTaken />
+      </Box>
     </Box>
-  </Box>
+  </ThemeProvider>
 );
 
 export default Dashboard;
