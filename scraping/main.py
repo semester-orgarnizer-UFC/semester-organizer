9bf44f80b4eea29ae643de27a9ce52e022530634
Renê <renejr.arraes286@gmail.com>
2023-08-07 14:52:12 -0300

    running api on different port

--- scraping/main.py
+++ scraping/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 from login_dto import LoginDto
 from scraping_s3 import scraping
+import uvicorn
 
 app = FastAPI()
 
 @app.post("/scraping")
 async def create_item(loginDto: LoginDto):
     return scraping(loginDto.login, loginDto.password)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8888)
